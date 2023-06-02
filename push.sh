9feefc6283c4234a1fa2c0baeb1bf11e5c929888
Moe-hacker <moe-hacker@outlook.com>
2023-06-02 03:00:05 -0400

    site update 2023年 06月 02日 星期五 03:00:05 EDT

---
+++ push.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+BK_REPO=../blog_backup
+cp -rv ./public ${BK_REPO}/
+cp -rv ./source ${BK_REPO}/
+cp -v ./_config.yml ${BK_REPO}/
+cp -v ./node_modules/hexo-theme-butterfly/_config.yml  ${BK_REPO}/_config-butterfly.yml
+cp -v ./package*.json ${BK_REPO}/
+cp -rv ./push.sh ${BK_REPO}/
+cd ${BK_REPO}
+git add .
+git commit -m "site update $(date)"
+git push
+./node_modules/hexo/bin/hexo g
+./node_modules/hexo/bin/hexo d
